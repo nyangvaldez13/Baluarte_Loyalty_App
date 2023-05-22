e4830631a07327972e978eaf99d044f6889c30a2
nyangvaldez13 <122779622+nyangvaldez13@users.noreply.github.com>
2023-05-22 21:43:36 +0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\xampp\\htdocs\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\xampp\\htdocs\\baluarte_loyalty_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\xampp\\htdocs\\flutter"
+  "PROJECT_DIR=C:\\xampp\\htdocs\\baluarte_loyalty_app"
+  "FLUTTER_ROOT=C:\\xampp\\htdocs\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\xampp\\htdocs\\baluarte_loyalty_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\xampp\\htdocs\\baluarte_loyalty_app"
+  "FLUTTER_TARGET=C:\\xampp\\htdocs\\baluarte_loyalty_app\\lib\\baluarte_rewards\\register_b.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\xampp\\htdocs\\baluarte_loyalty_app\\.dart_tool\\package_config.json"
+)
